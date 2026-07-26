71aa6d92876c670326103c37433fb09c8ff7ebe4
PEDRO ALEX <132845901+PadekZ@users.noreply.github.com>
2026-07-26 10:38:39 -0300

    Player Animation Finished

--- Assets/Scripts/Player/PlayerAnimator.cs
+++ Assets/Scripts/Player/PlayerAnimator.cs
@@ -1,3 +1,5 @@
+using System.Threading;
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,6 +9,10 @@ public class PlayerAnimator : MonoBehaviour {
     [SerializeField] private CharacterController characterController;
     [SerializeField] private NavMeshAgent navMeshAgent;
     [SerializeField] private PlayerMovement playerMovement;
+    private bool isTalking = false;
+    private bool isInReaction = false;
+    private float currentReactingTimer = 0f;
+    private int currentReaction;
 
     // Hashes para acesso performático dos parâmetros do Animator
     private readonly int speedHash = Animator.StringToHash("Speed");
@@ -21,6 +27,7 @@ public class PlayerAnimator : MonoBehaviour {
 
     private void Update() {
         UpdateMovementAnimation();
+        if (isTalking) { ConversationReactions(); }
     }
 
     private void UpdateMovementAnimation() {
@@ -42,14 +49,58 @@ public class PlayerAnimator : MonoBehaviour {
 
         // Atualiza a velocidade no Animator com suavização (damping)
         if (animator != null) {
-            Debug.Log(normalizedSpeed);
             animator.SetFloat(speedHash, normalizedSpeed, 0.1f, Time.deltaTime);
         }
     }
 
-    public void SetTalking(bool isTalking) {
+    public void SetTalking(bool _isTalking) {
         if (animator != null) {
-            animator.SetBool(isTalkingHash, isTalking);
+            animator.SetBool(isTalkingHash, _isTalking);
+            isTalking = _isTalking;
+        }
+    }
+
+    public void ConversationReactions() {
+        Debug.Log("Conversando");
+        if (!isInReaction) {
+            var reaction = Random.Range(0, 7);
+
+            if (reaction != currentReaction) {
+                currentReaction = reaction;
+                switch (reaction) {
+                    case 0:
+                        animator.SetTrigger("Inter_Talking");
+                        break;
+                    case 1:
+                        animator.SetTrigger("Inter_Closed");
+                        break;
+                    case 2:
+                        animator.SetTrigger("Inter_Sad");
+                        break;
+                    case 3:
+                        animator.SetTrigger("Inter_Angry");
+                        break;
+                    case 4:
+                        animator.SetTrigger("Inter_Exclamation");
+                        break;
+                    case 5:
+                        animator.SetTrigger("Inter_Happy");
+                        break;
+                    case 6:
+                        animator.SetTrigger("Inter_Question");
+                        break;
+                }
+
+                isInReaction = true;
+            }
+        } else {
+            currentReactingTimer += Time.deltaTime;
+            Debug.Log("Timer: " + currentReactingTimer);
+
+            if (currentReactingTimer >= 3f) {
+                isInReaction = false;
+                currentReactingTimer = 0f;
+            }
         }
     }
 }
