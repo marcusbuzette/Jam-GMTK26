6252cc1d55e2ec4a2ae81b1e3ebe6816c7255bcd
Felipe <felipecamposap@gmail.com>
2026-07-26 13:48:52 -0300

    build

--- Assets/Scripts/SO Definitions/LevelSetupSO.cs
+++ Assets/Scripts/SO Definitions/LevelSetupSO.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections.Generic;
-using Microsoft.Unity.VisualStudio.Editor;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "LevelSetupSO", menuName = "Scriptable Objects/LevelSetupSO")]
